1f919284848c1e2172d770bad7c950286ffc68e2
gtarun8341 <gtarun8341@gmail.com>
2025-04-05 10:39:41 +0530

    added new systems

--- src/components/apiConfig.js
+++ src/components/apiConfig.js
@@ -3,7 +3,9 @@ import axios from "axios";
 // Create an Axios instance with a base URL
 const api = axios.create({
   // baseURL: "http://localhost:5000/api", // Replace with your API's base URL
-  baseURL: "https://shedular-backend.vercel.app/api",
+  // baseURL: "https://shedular-backend.vercel.app/api",
+  baseURL: "https://shedular-backend-mocha.vercel.app/api",
+
 });
 
 export default api;
