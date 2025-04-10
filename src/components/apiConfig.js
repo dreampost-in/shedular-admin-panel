e4e6b7828e35547375f1691922a4b01bdbf97de6
gtarun8341 <gtarun8341@gmail.com>
2025-04-10 14:02:59 +0530

    new domain

--- src/components/apiConfig.js
+++ src/components/apiConfig.js
@@ -4,7 +4,8 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "http://localhost:5000/api", // Replace with your API's base URL
   // baseURL: "https://shedular-backend.vercel.app/api",
-  baseURL: "https://shedular-backend-mocha.vercel.app/api",
+  // baseURL: "https://shedular-backend-mocha.vercel.app/api",
+  baseURL: "https://api.dreampost.in/api",
 
 });
 
